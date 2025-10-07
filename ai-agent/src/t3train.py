c4741411dbc7c3e21a129b266b7db6d9de994ce6
thasan01 <thasan2004@yahoo.com>
2025-10-06 23:20:45 -0400

    inference evaluation during training

--- ai-agent/src/t3train.py
+++ ai-agent/src/t3train.py
@@ -44,6 +44,10 @@ def request_shutdown(base_url: str):
 
 
 def run_games(epoch, session_template, max_sessions, exploration_rate, player_id, swap_players):
+    global p1_profile
+    global p2_profile
+    global data_dir
+
     players = [p1_profile, p2_profile]
     curr_player_id = player_id
 
@@ -52,7 +56,7 @@ def run_games(epoch, session_template, max_sessions, exploration_rate, player_id
 
         subprocess.run(
             "node ./game/build/tic-tac-toe.console.js {} {} --outdir {} --suppressOutput --sessionName {} --encoder OneHotEncoder --explorationRate {}".format(
-                players[0], players[1],data_dir, session, exploration_rate),
+                players[0], players[1], data_dir, session, exploration_rate),
             shell=True,
             capture_output=True,
             text=True
@@ -62,12 +66,46 @@ def run_games(epoch, session_template, max_sessions, exploration_rate, player_id
             players[0], players[1] = players[1], players[0]
             curr_player_id = (curr_player_id % 2) + 1
 
+def calculate_session_stats(stats, player_id, winner, status_msg, filename):
+    if winner is None:
+        if "draw" in status_msg:
+            stats["draws"] += 1
+            stats["sessions"][1].append(filename)
+    elif winner == player_id:
+        stats["wins"] += 1
+        stats["sessions"][0].append(filename)
+    else:
+        stats["losses"] += 1
+        stats["sessions"][2].append(filename)
+    return
+
 
 def onehot_encode_state(action, player_id:int):
     onehot_player = [0, 0, 0]
     onehot_player[agent_player_id] = 1
     return action["board"] + onehot_player
 
+def eval_model(epoch):
+    global data_dir
+    eval_template = "eval-{:06d}-{:06d}-{:01d}"
+    run_games(epoch, eval_template, max_sessions//2, 0, 1, False)
+    run_games(epoch, eval_template, max_sessions//2, 0, 2, False)
+
+    eval_stats = {"wins": 0, "losses": 0, "draws": 0, "sessions": [[], [], []]}
+    file_pattern = re.compile("training-(.*).txt")
+
+    for root, _, files in os.walk(data_dir):
+        for filename in files:
+            if file_pattern.match(filename):
+                file_path = os.path.join(root, filename)
+                with open(file_path) as file:
+                    parsed_json = json.load(file)
+                    current_player = int(filename.split('-')[-1].split('.')[0])
+                    winner = parsed_json["winner"] if "winner" in parsed_json else None
+                    calculate_session_stats(eval_stats, current_player, winner, parsed_json["status"], filename)
+                os.remove(file_path)
+
+    print(f"EVAL epoch: {epoch} wins: {dataset.stats["wins"]}, losses: {dataset.stats["losses"]}, draws: {dataset.stats["draws"]}")
 
 class T3DQLDataset(Dataset):
     def __init__(self, root_dir: str, file_expression: str = ".*", delete_training_files=True, exploration_rate=0.0, exploration_decay = 0.0, experience_replay = 0.5):
@@ -84,22 +122,7 @@ class T3DQLDataset(Dataset):
         self.exp_rate_range = [0.01, 1.0]
 
     def __reset_stats(self):
-        # self.stats = {"p1_wins": 0, "p2_wins": 0, "draws": 0, "p1_dqs": 0, "p2_dqs": 0}
         self.stats = {"wins": 0, "losses": 0, "draws": 0, "sessions": [[],[],[]]}
-
-    def __calculate_session_stats(self, player_id, winner, status_msg, filename):
-        if winner is None:
-            if "draw" in status_msg:
-                self.stats["draws"] += 1
-                self.stats["sessions"][1].append(filename)
-        elif winner == player_id:
-            self.stats["wins"] += 1
-            self.stats["sessions"][0].append(filename)
-        else:
-            self.stats["losses"] += 1
-            self.stats["sessions"][2].append(filename)
-        return
-
 
     def pre_step(self, epoch: int):
         # reset memories and board states
@@ -132,7 +155,7 @@ class T3DQLDataset(Dataset):
                 if swap_players:
                     current_player = int(filename.split('-')[-1].split('.')[0])
                     
-                self.__calculate_session_stats(current_player, winner, parsed_json["status"], filename)
+                calculate_session_stats(self.stats, current_player, winner, parsed_json["status"], filename)
 
                 for act_idx, action in enumerate(history):
                     curr_choice = action["choice"]
@@ -249,6 +272,8 @@ if __name__ == "__main__":
     policy_sync_rate = init_config.get("policy_sync_rate",10)
     experience_replay = init_config.get("experience_replay",0.5)
     experience_retain_dist = init_config.get("experience_retain_dist",[0.8, 0.1, 0.1])
+    eval_rate = init_config.get("eval_rate",None)
+
 
     min_lr, max_lr = learn_rate_range
     # exp_rate_range = [0.01, 1.0]  # [min_value, max_value]
@@ -373,7 +398,7 @@ if __name__ == "__main__":
         tb_log.add_scalar('Loss/train', avg_loss, epoch)
         tb_log.add_scalar('Exp Rate/train', dataset.exploration_rate, epoch)
         tb_log.add_scalar('Learn Rate/train', scheduler.get_last_lr()[0], epoch)
-        print(f"epoch: {epoch} loss: {avg_loss}, learn_rate: {scheduler.get_last_lr()[0]:.2e}, exp_rate: {dataset.exploration_rate}, wins: {dataset.stats["wins"]}, losses: {dataset.stats["losses"]}, draws: {dataset.stats["draws"]}")
+        print(f"TRAINING epoch: {epoch} loss: {avg_loss}, learn_rate: {scheduler.get_last_lr()[0]:.2e}, exp_rate: {dataset.exploration_rate}, wins: {dataset.stats["wins"]}, losses: {dataset.stats["losses"]}, draws: {dataset.stats["draws"]}")
 
         dataset.post_step()
 
@@ -382,6 +407,9 @@ if __name__ == "__main__":
 
         if epoch % archive_rate == 0:
             archive_model(model_dir, archive_dir)
+
+        if eval_rate and epoch % eval_rate == 0:
+            eval_model(epoch)
 
     if avg_loss:
         t3target_dqn.load_state_dict(t3policy_dqn.state_dict())
